fde7c1509b60c641e3f5fb218e57616219f84a84
Vincent Liu <zl2474@columbia.edu>
2018-01-10 23:27:23 -0800

    edited flask for zl2474 template rendering

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/zl2474')
+def zl2474():
+    return render_template('zl2474.html')
+
 
 if __name__ == '__main__':
     app.run()
